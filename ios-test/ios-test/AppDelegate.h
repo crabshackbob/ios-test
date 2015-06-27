0ca7b1367d0e375c072cfdaad76c53e1fe490d68
Young <younr076@sb-younr076-ml1.attlocal.net>
2015-06-26 21:53:55 -0700

    initial commit

---
+++ ios-test/ios-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-test
+//
+//  Created by Young, Bob on 6/26/15.
+//  Copyright (c) 2015 Young, Bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
